c43fd3238aaafdcedf84368a80b2662213822bc5
Laurent Della-Negra <laurent.dellanegra@gmail.com>
2022-08-21 16:57:57 +0200

    🐛 Fix scope PWA

--- next.config.js
+++ next.config.js
@@ -20,7 +20,7 @@ module.exports = withPWA({
   pwa: {
     // disable: process.env.NODE_ENV === 'development',
     dest: 'public',
-    scope: '/src',
+    scope: '/',
     runtimeCaching,
   },
 })
