46eeb78b4dd8a584d5c0eb7362cee221551a0bc6
Laurent Della-Negra <laurent.dellanegra@gmail.com>
2022-08-21 13:58:39 +0200

    🔍 Add favicon and meta header

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,21 @@ import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="fr" className="h-screen bg-pattern text-text-and-icon">
+    <Html lang="en" className="h-screen bg-pattern text-text-and-icon">
       <Head>
+        {/* Favicon */}
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#1b2a32" />
+        <meta name="msapplication-TileColor" content="#1b2a32" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta
+          name="description"
+          content="
+          Gold Premium Calculator is a tool that helps investors calculate the premium on a bar, coin or any form of precious metal"
+        />
         {/* Font */}
         <link
           rel="preload"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next'
 import Image from 'next/future/image'
+import Head from 'next/head'
 import { ChangeEvent, useState } from 'react'
 import { QueryClient, dehydrate, useQuery } from 'react-query'
 
@@ -24,116 +25,117 @@ const Home: NextPage = () => {
   const market = marketData?.oneGram || 0
   const realWeight = (weight * quality) / 100
   const premium = ((price - realWeight * market) / (realWeight * market)) * 100
-  const displayPremium = premium && premium > -100 ? `${premium.toFixed(2)}%` : ''
+  const displayPremium = quality && premium && premium > -100 ? `${premium.toFixed(2)}%` : ''
 
   const handleWeight = (e: ChangeEvent<HTMLInputElement>) => setWeight(Number(e.target.value))
   const handleQuality = (e: ChangeEvent<HTMLInputElement>) => setQuality(Number(e.target.value))
   const handlePrice = (e: ChangeEvent<HTMLInputElement>) => setPrice(Number(e.target.value))
 
   return (
-    <div className="p-4">
-      <Card.Container className="max-w-md m-auto">
-        <Card.Content>
-          <H1>
-            <Image src={GoldLogo} alt="Gold" className="h-16 w-auto" priority unoptimized />
-          </H1>
+    <>
+      <Head>
+        <title>Gold Premium Calculator</title>
+      </Head>
+      <div className="p-4">
+        <Card.Container className="max-w-md m-auto">
+          <Card.Content>
+            <H1>
+              <Image src={GoldLogo} alt="Gold" className="h-16 w-auto" priority unoptimized />
+            </H1>
 
-          <div className="flex flex-col gap-4">
-            <GroupInput className="flex gap-1 flex-wrap">
-              <div className="flex flex-1 items-center justify-end">
-                <Label htmlFor="market" className="mr-2">
-                  Cours de l&apos;or :
-                </Label>
-              </div>
-              <div className="flex flex-1 items-center justify-start">
-                {market ? `${market} €` : '--'}
-                {isFetching ? ' Refreshing...' : null}
-              </div>
-            </GroupInput>
+            <div className="flex flex-col gap-4">
+              <GroupInput className="flex gap-1 flex-wrap">
+                <div className="flex flex-1 items-center justify-end">
+                  <Label className="mr-2">Gold market price :</Label>
+                </div>
+                <div className="flex flex-1 items-center justify-start">
+                  {market ? `${market} €` : '--'}
+                  {isFetching ? ' Refreshing...' : null}
+                </div>
+              </GroupInput>
 
-            <GroupInput className="flex gap-1">
-              <div className="flex flex-1 items-center justify-end">
-                <Label htmlFor="weight" className="mr-2">
-                  Poids brut :
-                </Label>
-              </div>
-              <div className="flex flex-1 items-center justify-start">
-                <Input
-                  id="weight"
-                  name="Weight"
-                  type="number"
-                  placeholder="31.1"
-                  className="max-w-[128px]"
-                  value={weight || ''}
-                  onChange={handleWeight}
-                  min={0}
-                />
-                <span className="group-focus-within:text-gold">g</span>
-              </div>
-            </GroupInput>
+              <GroupInput className="flex gap-1">
+                <div className="flex flex-1 items-center justify-end">
+                  <Label htmlFor="weight" className="mr-2">
+                    Total weight :
+                  </Label>
+                </div>
+                <div className="flex flex-1 items-center justify-start">
+                  <Input
+                    id="weight"
+                    name="Weight"
+                    type="number"
+                    placeholder="31.1"
+                    className="max-w-[128px]"
+                    value={weight || ''}
+                    onChange={handleWeight}
+                    min={0}
+                  />
+                  <span className="group-focus-within:text-gold">g</span>
+                </div>
+              </GroupInput>
 
-            <GroupInput className="flex gap-1">
-              <div className="flex flex-1 items-center justify-end">
-                <Label htmlFor="quality" className="mr-2">
-                  Quality :
-                </Label>
-              </div>
-              <div className="flex flex-1 items-center justify-start">
-                <Input
-                  id="quality"
-                  name="Quality"
-                  type="number"
-                  placeholder="100"
-                  className="max-w-[80px]"
-                  value={quality || ''}
-                  onChange={handleQuality}
-                  min={0}
-                  max={100}
-                />
-                <span className="group-focus-within:text-gold">%</span>
-              </div>
-            </GroupInput>
+              <GroupInput className="flex gap-1">
+                <div className="flex flex-1 items-center justify-end">
+                  <Label htmlFor="quality" className="mr-2">
+                    Purity :
+                  </Label>
+                </div>
+                <div className="flex flex-1 items-center justify-start">
+                  <Input
+                    id="quality"
+                    name="Quality"
+                    type="number"
+                    placeholder="100"
+                    className="max-w-[80px]"
+                    value={quality || ''}
+                    onChange={handleQuality}
+                    min={0}
+                    max={100}
+                  />
+                  <span className="group-focus-within:text-gold">%</span>
+                </div>
+              </GroupInput>
 
-            <GroupInput className="flex gap-1">
-              <div className="flex flex-1 items-center justify-end">
-                <Label htmlFor="quality" className="mr-2">
-                  Poids fin :
-                </Label>
-              </div>
-              <div className="flex flex-1 items-center justify-start">
-                <div>{realWeight}g</div>
-              </div>
-            </GroupInput>
+              <GroupInput className="flex gap-1">
+                <div className="flex flex-1 items-center justify-end">
+                  <Label className="mr-2">Real weight :</Label>
+                </div>
+                <div className="flex flex-1 items-center justify-start">
+                  <div>{realWeight}g</div>
+                </div>
+              </GroupInput>
 
-            <GroupInput className="flex gap-1">
-              <div className="flex flex-1 items-center justify-end">
-                <Label htmlFor="weight" className="mr-2">
-                  Prix de vente :
-                </Label>
-              </div>
-              <div className="flex flex-1 items-center justify-start">
-                <Input
-                  id="price"
-                  name="Price"
-                  type="number"
-                  placeholder="180"
-                  className="max-w-[128px]"
-                  value={price || ''}
-                  onChange={handlePrice}
-                  min={0}
-                />
-                <span className="group-focus-within:text-gold">€</span>
-              </div>
-            </GroupInput>
-          </div>
-        </Card.Content>
+              <GroupInput className="flex gap-1">
+                <div className="flex flex-1 items-center justify-end">
+                  <Label htmlFor="price" className="mr-2">
+                    Price :
+                  </Label>
+                </div>
+                <div className="flex flex-1 items-center justify-start">
+                  <Input
+                    id="price"
+                    name="Price"
+                    type="number"
+                    placeholder="180"
+                    className="max-w-[128px]"
+                    value={price || ''}
+                    onChange={handlePrice}
+                    min={0}
+                  />
+                  <span className="group-focus-within:text-gold">€</span>
+                </div>
+              </GroupInput>
+            </div>
+          </Card.Content>
 
-        {/* Card footer */}
-        <Card.Footer className="text-center text-xl">
-          Prime : <span className="text-gold">{displayPremium || '--'}</span>
-        </Card.Footer>
-      </Card.Container>
-    </div>
+          {/* Card footer */}
+          <Card.Footer className="text-center text-xl">
+            Premium : <span className="text-gold">{displayPremium || '--'}</span>
+          </Card.Footer>
+        </Card.Container>
+      </div>
+    </>
   )
 }
 
