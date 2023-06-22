92925447f2f2b4d45def52a5939eeec944541d4e
laurentDellaNegra <laurent.dellanegra@gmail.com>
2023-06-22 10:14:12 +0200

    👽️ Change API to fetch gold price

--- src/lib/market.ts
+++ src/lib/market.ts
@@ -5,11 +5,11 @@ const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || ''
 export async function getGoldMarket({ queryKey }: QueryFunctionContext<[string, string]>) {
   const [, queryParam] = queryKey
   const url = `${SERVER_URL}/api/gold/${queryParam}`
-  console.log(url)
+  // console.log(url)
   try {
     const response = await fetch(url)
     const content = await response.json()
-    console.log('content', content)
+    // console.log('content', content)
     return content
   } catch (error) {
     console.error(error)
--- src/pages/api/gold/[currency].ts
+++ src/pages/api/gold/[currency].ts
@@ -1,3 +1,4 @@
+import { load } from 'cheerio'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL || ''
@@ -19,25 +20,66 @@ const TwoHourAgo = (currentTimeStamp: number) => {
   return currentTimeStamp > hourago
 }
 
+const OneHourAgo = (currentTimeStamp: number) => {
+  const hour = 1000 * 60 * 60
+  const hourago = Date.now() - hour
+
+  return currentTimeStamp > hourago
+}
+
 // Very simple cache to limit API calls (limit: 500 calls per months max)
 const CONTENT_CACHED = new Map()
 
-async function getGoldMarket(currency: string) {
+async function getGoldMarketGoldApi(currency: string) {
   const url = `${API_URL}/api/XAU/${currency}`
   let contentCached = CONTENT_CACHED.get(url)
   try {
     if (!contentCached || TwoHourAgo(contentCached?.timestamp)) {
-      console.log('Data fetched')
+      // console.log('Data fetched')
       const response = await fetch(url, OPTIONS as any)
       const json = await response.json()
-      console.log(json)
+      // console.log(json)
       CONTENT_CACHED.set(url, json)
       contentCached = CONTENT_CACHED.get(url)
     } else {
-      console.log('Data from cache')
+      // console.log('Data from cache')
     }
     return {
       oneGram: contentCached.price_gram_24k,
+    }
+  } catch (error) {
+    console.error(error)
+    if (error instanceof Error) {
+      // return { error: error.message }
+    } else {
+      // return { error: String(error) }
+    }
+    return { oneGram: 0 }
+  }
+}
+
+// crawl https://www.livepriceofgold.com/
+async function getGoldMarket(currency: string) {
+  // console.log('currency', currency)
+  const url = 'https://www.livepriceofgold.com/'
+  let contentCached = CONTENT_CACHED.get(url)
+  try {
+    if (!contentCached || OneHourAgo(contentCached?.timestamp)) {
+      // console.log('Data fetched')
+      const response = await fetch(url, OPTIONS as any)
+      const text = await response.text()
+      const $ = load(text)
+      const EUR = Number($('span[data-price="GXAUUSD_EUR"]').first().text().trim())
+      const USD = Number($('span[data-price="GXAUUSD"]').first().text().trim())
+      const CHF = Number($('span[data-price="GXAUUSD_CHF"]').first().text().trim())
+
+      CONTENT_CACHED.set(url, { EUR, USD, CHF })
+      contentCached = CONTENT_CACHED.get(url)
+    } else {
+      // console.log('Data from cache')
+    }
+    return {
+      oneGram: contentCached[currency],
     }
   } catch (error) {
     console.error(error)
